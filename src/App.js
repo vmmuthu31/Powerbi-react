6c383fd9621ff7f79dc707af8d25eae307b496c4
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-14 14:58:41 +0530

    added the import from the powerbi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { PowerBIEmbed } from "powerbi-client-react";
 
 function App() {
   return (
